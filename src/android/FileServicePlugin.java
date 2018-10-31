32e6c827b81023b271add0fd7976e02b1ccbf206
Boyina Srinivasa Rao <boyinasrinivasulu@gmail.com>
2018-10-31 10:12:35 +0530

    Added android support

---
+++ src/android/FileServicePlugin.java
@@ -0,0 +1,181 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+package com.wavemaker.cordova.plugin;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class FileServicePlugin extends CordovaPlugin {
+    private static final String TAG = "FILE";
+
+    //PERMISSION REQUEST CODE
+    private static final int WRITE_PERMISSION_REQUEST_CODE = 1000;
+
+    //ACTIVITY REQUEST CODE
+    private static final int FILE_REQUEST_CODE = 1000;
+
+    //STATE PROPERTY KEYS
+    private static final String STATE_PROPERTY_MIME_TYPE = "mimeType";
+    private static final String STATE_PROPERTY_MULTIPLE = "multiple";
+
+    //MESSAGES
+    private static final String PERMISSION_DENIED = "1";
+    private static final String ERROR_HAPPENED = "2";
+
+    private String mimeType;
+    private boolean multiple;
+    private CallbackContext mCallbackContext;
+
+    /**
+     * Constructor.
+     */
+    public FileServicePlugin() {
+        Log.d(TAG, "File plugin instance created");
+    }
+
+    @Override
+    public Bundle onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putString(STATE_PROPERTY_MIME_TYPE, mimeType);
+        bundle.putBoolean(STATE_PROPERTY_MULTIPLE, multiple);
+        return bundle;
+    }
+
+    @Override
+    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
+        super.onRestoreStateForActivityResult(state, callbackContext);
+        this.mimeType = state.getString(STATE_PROPERTY_MIME_TYPE);
+        this.multiple = state.getBoolean(STATE_PROPERTY_MULTIPLE);
+        this.mCallbackContext = callbackContext;
+    }
+
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("selectFiles".equals(action)) {
+            JSONObject options = args.getJSONObject(0);
+            this.mimeType = options.getString(STATE_PROPERTY_MIME_TYPE);
+            this.multiple = options.getBoolean(STATE_PROPERTY_MULTIPLE);
+            this.mCallbackContext = callbackContext;
+            if (!this.cordova.hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                this.cordova.requestPermission(this, 0, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+            this.selectFiles();
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        JSONArray files = new JSONArray();
+            if (requestCode == FILE_REQUEST_CODE) {
+                JSONObject result = new JSONObject();
+                if (resultCode == Activity.RESULT_OK) {
+                    ClipData clipData = data.getClipData();
+                    if (clipData != null) {
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            files.put("" + getRealPath(clipData.getItemAt(i).getUri()));
+                        }
+                    } else if (data.getData() != null) {
+                        files.put("" + getRealPath(data.getData()));
+                    }
+                    try {
+                        result.put("files", files);
+                        this.mCallbackContext.success(result);
+                    } catch (JSONException jse) {
+                        this.mCallbackContext.error(ERROR_HAPPENED);
+                    } finally {
+                        this.mCallbackContext = null;
+                    }
+                }
+            }
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        super.onRequestPermissionResult(requestCode, permissions, grantResults);
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                this.mCallbackContext.error(PERMISSION_DENIED);
+                this.mCallbackContext = null;
+                return;
+            }
+        }
+        if (requestCode == WRITE_PERMISSION_REQUEST_CODE) {
+            selectFiles();
+        }
+    }
+
+
+
+    private void selectFiles() {
+        Intent showBrowser = new Intent();
+        showBrowser.setAction(Intent.ACTION_GET_CONTENT);
+        showBrowser.addCategory(Intent.CATEGORY_OPENABLE);
+        showBrowser.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, multiple);
+        showBrowser.setType(mimeType);
+        this.cordova.startActivityForResult(this, showBrowser, FILE_REQUEST_CODE);
+    }
+
+    private String getRealPath(Uri uri) {
+        String path = FileHelper.getRealPath(uri, this.cordova.getActivity());
+        try {
+            if (path == null) {
+                InputStream inStream = FileHelper.getInputStreamFromUriString(uri.toString(), this.cordova.getActivity());
+                java.io.File temp = java.io.File.createTempFile("temp", "_" + System.currentTimeMillis(), this.cordova.getContext().getCacheDir());
+                OutputStream oStream = new FileOutputStream(temp);
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = inStream.read(buffer)) != -1) {
+                    oStream.write(buffer, 0, len);
+                }
+                path = temp.getAbsolutePath();
+            }
+        } catch (Exception e) {
+            Log.e("FILE_CHOOSER", "Failed to resolve real path", e);
+        }
+        return path == null || path.startsWith("file://") ? path: "file://" + path;
+    }
+}
+
---
+++ www/fileService.js
@@ -0,0 +1,85 @@
+cordova.define("cordova-plugin-wm-file.fileService", function(require, exports, module) {
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+var exec = require('cordova/exec');
+
+///channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+//channel.waitForInitialization('onCordovaInfoReady');
+
+/**
+ * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
+ * phone, etc.
+ * @constructor
+ */
+function FileService () {
+
+}
+
+/**
+ * Get device info
+ *
+ * @param {Function} successCallback The function to call when the heading data is available
+ * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
+ */
+FileService.prototype.select = function (options, successCallback, errorCallback) {
+    var DEFAULT_OPTIONS = {
+        'mimeType': '*/*',
+        'multiple': false
+    };
+    options = options || {};
+    for (key in DEFAULT_OPTIONS) {
+        if (options[key] === undefined) {
+            options[key] = DEFAULT_OPTIONS[key];
+        }
+    }
+    exec(successCallback, errorCallback, 'FileService', 'selectFiles', [options]);
+};
+
+FileService.prototype.selectImage = function(multiple, successCallback, errorCallback) {
+    var options = {
+        "mimeType": "image/*",
+        "multiple": multiple
+    };
+    this.select(options, successCallback, errorCallback);
+};
+
+FileService.prototype.selectVideo = function(multiple, successCallback, errorCallback) {
+    var options = {
+        "mimeType": "video/*",
+        "multiple": multiple
+    };
+    this.select(options, successCallback, errorCallback);
+};
+
+FileService.prototype.selectAudio = function(multiple, successCallback, errorCallback) {
+    var options = {
+        "mimeType": "audio/*",
+        "multiple": multiple
+    };
+    this.select(options, successCallback, errorCallback);
+};
+
+module.exports = new FileService();
+
+});
+
